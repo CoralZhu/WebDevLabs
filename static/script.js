e775881d942a9df70bf5c5515c8a62eccc5953d9
CoralZhu <xiangcuz@andrew.cmu.edu>
2025-03-23 15:29:13 -0400

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+    $("#longIntro").hide();
+    $("#readMore").click(function(){
+        $("#longIntro").slideDown();
+        $("#readMore").hide();
+    });
+    $("#readLess").click(function(){
+        $("#longIntro").slideUp();
+        $("#readMore").show();
+    });
+});
+
 let x=5;
 let y=7;
 let z=x+y;
@@ -21,7 +33,7 @@ else if (C.length<z.length){
 else{
     console.log("good job!");
 }
-let L1=["Watermelon","Pineapple","Pear","Banana"];
+/* let L1=["Watermelon","Pineapple","Pear","Banana"];
 let L2=["Apple","Banana","Kiwi","Orange"];
 function findTheBanana(arr,name){
     arr.forEach(function(item,index){
@@ -31,20 +43,20 @@ function findTheBanana(arr,name){
     });
 }
 findTheBanana(L1,"L1");
-findTheBanana(L2,"L2");
+findTheBanana(L2,"L2"); */
 function greetingFunc(){
     let d=new Date;
     let h=d.getHours();
-    if (h<12){
+    if (h>=5 && h<12){
         greetingMessage="Good morning";
     }
-    else if (h<18){
+    else if (h>=12 && h<18){
         greetingMessage="Good afternoon";
     }
-    else if (h<20){
+    else if (h>=18 && h<20){
         greetingMessage="Good evening";
     }
-    else if ((h>20 && h<24) || (h>0 && h<5)){
+    else if ((h>20 && h<24) || (h>=0 && h<5)){
         greetingMessage="Good night";
     }
     let E=document.getElementById("greeting");
@@ -52,8 +64,56 @@ function greetingFunc(){
         E.innerHTML=`${greetingMessage}, I am Coral Zhu`;
     }
 }
-greetingFunc();
+/* greetingFunc();
 window.onload=greetingFunc;
 if (window.location.pathname.endsWith("index.html") || window.location.pathname == "/"){
     window.onload=greetingFunc;
+} */
+function addYear(){
+    const currentYear = new Date().getFullYear();
+    document.getElementById("copyYear").textContent = currentYear;
+}
+// function showList(){
+//     const list=document.getElementById('funList');
+//     const button=document.getElementById('showListButton');
+//     list.style.display='block';
+//     button.style.display='none';
+// }
+
+function validate() {
+    const form=document.getElementById("contactForm");
+
+    const errorMessage=document.getElementById("errorMessage");
+    // errorMessage.innerHTML="Please fill out the form correctly so I can get back to you :)";
+    
+        
+    if (!form.checkValidity()) {
+        // event.preventDefault();
+        /* form.reportValidity(); */
+        errorMessage.innerHTML="Please fill out the form correctly so I can get back to you :)";
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form=document.getElementById("contactForm");
+    const errorMessage=document.getElementById("errorMessage");
+    errorMessage.innerHTML="Please fill out the form correctly so I can get back to you :)";
+    // form.addEventListener("submit",function(event) {
+    //     errorMessage.textContent= "";
+    //     if (!form.checkValidity()) {
+    //         event.preventDefault();
+    //         /* form.reportValidity(); */
+    //         errorMessage.innerHTML="Please fill out the form correctly so I can get back to you :)";
+    //     }
+    // });
+});
+function getAdvice(){
+    fetch("https://api.adviceslip.com/advice").then(response => response.json()).then(data => {
+        document.getElementById("adviceText").innerText=data.slip.advice;
+    })
+    .catch(error => {
+        console.error("Error fetching advice:",error);
+        document.getElementById("adviceText").innerText="Oops, something went wrong. Please try again later.";
+    })
 }
